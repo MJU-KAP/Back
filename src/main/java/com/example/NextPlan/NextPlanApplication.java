2cae7cab10208da3b1e87179c749f7ba1e2afeda
SangHyuk <tkdgur1730@gmail.com>
2026-05-01 23:31:03 +0900

    feat: 로그인 인증 및 크롤링 API 수정

--- src/main/java/com/example/NextPlan/NextPlanApplication.java
+++ src/main/java/com/example/NextPlan/NextPlanApplication.java
@@ -2,8 +2,10 @@ package com.example.NextPlan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NextPlanApplication {
 
 	public static void main(String[] args) {
