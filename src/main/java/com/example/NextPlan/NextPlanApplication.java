6fff2866e68b44eb51ad2609e3c92f875c9550b6
ZZGG2 <tkdxm0208@gmail.com>
2026-05-04 20:56:40 +0900

    Enable CORS configuration in NextPlanApplication

--- src/main/java/com/example/NextPlan/NextPlanApplication.java
+++ src/main/java/com/example/NextPlan/NextPlanApplication.java
@@ -1,15 +1,16 @@
 package com.example.NextPlan;
 
+import com.example.NextPlan.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableConfigurationProperties(CorsConfig.class)
 public class NextPlanApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(NextPlanApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(NextPlanApplication.class, args);
+    }
 }
